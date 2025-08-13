e8bdcb51bc01db3ce674998ba78140b85be1ed78
arunkumarayinabathina <arun987610@gmail.com>
2025-08-13 16:11:50 +0530

    operators

---
+++ operators/app.js
@@ -0,0 +1,75 @@
+// JavaScript operators
+
+// Operators are symbols used to perform operations on variables and values.
+
+// Types of Operators :
+// 1. Arithmetic Operators (+, -, *, /, %)
+// 2. Comparison Operators (==, !=, >, <, >=, <=)
+// 3. Logical Operators (&&, ||, !)
+// 4. Increment /Decrement Operators (++ , --)
+// 5. typeOf
+// 6. spread Operator
+// 7. Rest Operator
+// 8. Bitwise Operator
+// 9. Terinary Operator
+
+// Arithematic Operator
+
+// Addition
+let sum = 5 + 10; // 15
+
+// Subtraction
+let difference = 10 - 5; // 5
+
+// Multiplication
+let product = 5 * 10; // 50
+
+// Division
+let quotient = 10 / 5; // 2
+
+// Modulus
+let remainder = 10 % 3; // 1
+
+// Comparison operators
+
+// Equal to
+let isEqual = (5 == 5); // true
+
+// Not equal to
+let isNotEqual = (5 != 10); // true
+
+// Greater than
+let isGreater = (10 > 5); // true
+
+// Less than
+let isLess = (5 < 10); // true
+
+// Greater than or equal to
+let isGreaterOrEqual = (10 >= 10); // true
+
+// Less than or equal to
+let isLessOrEqual = (5 <= 10); // true
+
+
+// Logical Operators
+
+// AND
+let isTrue = (true && true); // true
+
+// OR
+let isFalse = (true || false); // true
+
+// NOT
+let isNotTrue = !true; // false
+
+// Increment or Decrement operators
+
+// Increment
+let count = 0;
+count++; // 1
+
+// Decrement
+count--; // 0
+
+
+
