108f4851eab61dea29655a372d9ab94025f0aa0e
arunkumarayinabathina <arun987610@gmail.com>
2025-09-18 22:02:10 +0530

    Resolved using callback function

--- promises/app.js
+++ promises/app.js
@@ -40,10 +40,11 @@ function getPosts(){
     },1000);
 }
 
-function createPost(post){
+function createPost(post,callback){
     setTimeout(()=>{
         posts.push(post)
+        callback();
     },2000)
 }
-getPosts();
-createPost({title:"Post three",body:"This is post three"})
+
+createPost({title:"Post three",body:"This is post three"},getPosts)
