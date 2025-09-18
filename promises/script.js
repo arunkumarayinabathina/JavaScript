72a092d0749ea404ef4a5e01e3ea2588c63f5922
arunkumarayinabathina <arun987610@gmail.com>
2025-09-18 22:19:30 +0530

    Promises in javascript

---
+++ promises/script.js
@@ -0,0 +1,45 @@
+const posts = [
+    {title:"Post one",body:"This is post one"},
+    {title:"Post two",body:"This is post two"}
+]
+
+function getPosts(){
+    setTimeout(()=>{
+        let output = "";
+        posts.forEach((post,index)=>{
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output
+    },1000);
+}
+
+function createPost(post){
+    return new Promise((res,rej)=>{
+        setTimeout(()=>{
+            posts.push(post)
+
+            const error = false;
+
+            if(!error){
+                res();
+            }
+            else{
+                rej(`Error something went wrong`);
+            }
+        },2000)
+    })
+}
+// createPost({title:"Post three",body:"This is post three"}).then(getPosts).catch((err)=>console.log(err));
+
+//Promise.all
+
+const promise1 = Promise.resolve("Hello world");
+const promise2 = 10;
+const promise3 = new Promise((res,rej)=>
+    setTimeout(res,2000,"Goodbye")
+)
+
+const promise4 = fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+const promise5 = fetch(`https://jsonplaceholder.typicode.com/posts`).then(res=>res.json())
+Promise.all([promise1,promise2,promise3,promise4,promise5]).then(values=>console.log(values))
